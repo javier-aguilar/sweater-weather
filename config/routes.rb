6df8db008a242bce6665ab1d85b68bd450c20843
Javier Aguilar <jvr.agx@gmail.com>
2020-06-07 17:50:01 -0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :forecast
+      resources :forecast, only: [:index]
+      resources :backgrounds, only: [:index]
     end
   end
 end
