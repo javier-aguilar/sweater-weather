3dc178e282da3d8781d86eab804d834ed3c3cdaa
Javier Aguilar <jvr.agx@gmail.com>
2020-06-08 10:41:21 -0600

    Add additional test to foodie request spec

--- spec/api/v1/foodie_request_spec.rb
+++ spec/api/v1/foodie_request_spec.rb
@@ -12,25 +12,26 @@ describe "API V1" do
       expect(data).to have_key :travel_time
       expect(data).to have_key :forecast
       expect(data).to have_key :restaurant
+
+      expect(data[:end_location]).to eq "pueblo,co"
+      expect(data[:travel_time]).to eq "1 hour 48 mins"
+
+      expect(data[:restaurant][:name]).to eq "Angelo's Pizza Parlor"
+      expect(data[:restaurant][:address]).to eq "105 E Riverwalk, Pueblo 81003"
+    end
+    it "returns food and forecast information for denver to austin", :vcr do
+      get '/api/v1/foodie?start=denver,co&end=austin,tx&search=mexican'
+
+      expect(response).to be_successful
+      json = JSON.parse(response.body, symbolize_names: true)
+      data = json[:data][:attributes]
+      expect(data).to have_key :end_location
+      expect(data).to have_key :travel_time
+      expect(data).to have_key :forecast
+      expect(data).to have_key :restaurant
+
+      expect(data[:end_location]).to eq "austin,tx"
+      expect(data[:travel_time]).to eq "14 hours 9 mins"
     end
   end
 end
-
-# {
-#   "data": {
-#     "id": "null",
-#     "type": "foodie",
-#     "attributes": {
-#       "end_location": "pueblo,co",
-#       "travel_time": "1 hours 48 min",
-#       "forecast": {
-#         "summary": "Cloudy with a chance of meatballs",
-#         "temperature": "83"
-#       },
-#       "restaurant": {
-#         "name": "Angelo's Pizza Parlor",
-#         "address": "105 E Riverwalk, Pueblo 81003"
-#       }
-#     }
-#   }
-# }
