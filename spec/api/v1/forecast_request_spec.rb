210282363bd438d5cd6500ed737bde2559eb7bce
Javier Aguilar <jvr.agx@gmail.com>
2020-06-07 23:56:17 -0600

    Update forecast request spec

--- spec/api/v1/forecast_request_spec.rb
+++ spec/api/v1/forecast_request_spec.rb
@@ -7,25 +7,55 @@ describe "API V1" do
 
       expect(response).to be_successful
       json = JSON.parse(response.body, symbolize_names: true)
-
-      expect(json[:data][:attributes].keys).to match_array([:location, :current, :hourly, :daily])
-      expect(json[:data][:attributes][:location]).to eq("Denver, CO, USA")
-
-      expect(json[:data][:attributes][:current][:sunrise]).to eq(1591529528)
-      expect(json[:data][:attributes][:current][:sunset]).to eq(1591583161)
-      expect(json[:data][:attributes][:current][:temp]).to eq(303.5)
-      expect(json[:data][:attributes][:current][:feels_like]).to eq(298.11)
-      expect(json[:data][:attributes][:current][:uvi]).to eq(11.08)
-      expect(json[:data][:attributes][:current][:weather].first[:main]).to eq("Clear")
-
-      expect(json[:data][:attributes][:hourly].first[:dt]).to eq(1591574400)
-      expect(json[:data][:attributes][:hourly].first[:temp]).to eq(303.5)
-      expect(json[:data][:attributes][:hourly].first[:weather].first[:main]).to eq("Clear")
-
-      expect(json[:data][:attributes][:daily].first[:dt]).to eq(1591552800)
-      expect(json[:data][:attributes][:daily].first[:temp][:min]).to eq(297.63)
-      expect(json[:data][:attributes][:daily].first[:temp][:max]).to eq(303.5)
-      expect(json[:data][:attributes][:daily].first[:weather].first[:main]).to eq("Clear")
+      data = json[:data][:attributes]
+      expect(data).to have_key :current
+      expect(data).to have_key :details
+      expect(data).to have_key :hourly
+      expect(data).to have_key :daily
     end
+  end
+  it "returns forecast data for seattle", :vcr do
+    get '/api/v1/forecast?location=seattle,wa'
+
+    expect(response).to be_successful
+    json = JSON.parse(response.body, symbolize_names: true)
+    data = json[:data][:attributes]
+    expect(data).to have_key :current
+    expect(data).to have_key :details
+    expect(data).to have_key :hourly
+    expect(data).to have_key :daily
+  end
+  it "returns forecast data for atlanta", :vcr do
+    get '/api/v1/forecast?location=atlanta+georgia'
+
+    expect(response).to be_successful
+    json = JSON.parse(response.body, symbolize_names: true)
+    data = json[:data][:attributes]
+    expect(data).to have_key :current
+    expect(data).to have_key :details
+    expect(data).to have_key :hourly
+    expect(data).to have_key :daily
+  end
+  it "returns forecast data for anchorage", :vcr do
+    get '/api/v1/forecast?location=anchorage'
+
+    expect(response).to be_successful
+    json = JSON.parse(response.body, symbolize_names: true)
+    data = json[:data][:attributes]
+    expect(data).to have_key :current
+    expect(data).to have_key :details
+    expect(data).to have_key :hourly
+    expect(data).to have_key :daily
+  end
+  it "returns forecast data for sydney", :vcr do
+    get '/api/v1/forecast?location=sydney,australia'
+
+    expect(response).to be_successful
+    json = JSON.parse(response.body, symbolize_names: true)
+    data = json[:data][:attributes]
+    expect(data).to have_key :current
+    expect(data).to have_key :details
+    expect(data).to have_key :hourly
+    expect(data).to have_key :daily
   end
 end
