0129e19c5f8f68a93c8ad4ba6d5dee9cea253f73
Javier Aguilar <jvr.agx@gmail.com>
2020-06-08 10:15:10 -0600

    Return responses on forecast and restaurant methods

--- app/poros/travel.rb
+++ app/poros/travel.rb
@@ -1,23 +1,30 @@
 class Travel
-  attr_reader :end_location, :duration
+  attr_reader :id, :end_location, :travel_time
 
   def initialize(info)
+    @id = 1
     @end_location = info[:end_location]
-    @duration = info[:duration]
+    @travel_time = info[:travel_time]
+    @location = Location.info(@end_location)
   end
 
   def self.get_info(startpoint, endpoint)
     travel_info = GoogleService.new.travel_time(startpoint, endpoint)
     info = { end_location: endpoint,
-             duration: travel_info[:routes][0][:legs][0][:duration][:text] }
+             travel_time: travel_info[:routes][0][:legs][0][:duration][:text] }
     Travel.new(info)
   end
 
   def forecast
-    # return summary and temperature
+    response = ForecastService.new.forecast_info(@location)
+    { summary: response[:current][:temp].to_i,
+      temperature: response[:current][:weather][0][:main] }
   end
 
   def restaurant
-    # return restaurant name and address
+    restaurant_info = RestaurantService.new.get_info(@location)
+    first_result = restaurant_info[:restaurants][0]
+    { name: first_result[:restaurant][:name],
+      address: first_result[:restaurant][:location][:address] }
   end
 end
