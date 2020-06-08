5976372f7c63a7171a33b2c184296a7929d3abff
Javier Aguilar <jvr.agx@gmail.com>
2020-06-08 14:15:21 -0600

    Add error method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,14 +1,23 @@
 class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(users_params)
-    binding.pry
     if user.save
-
+      render json: UserSerializer.new(user)
     else
+      message = user.errors.full_messages.to_sentence
+      error(message)
     end
   end
 
+  private
+
   def users_params
     params.permit(:email, :password, :password_confirmation)
+  end
+
+  def error(message)
+    render json: { status: :bad_request,
+                   code: 400,
+                   message: message }, status: :bad_request
   end
 end
