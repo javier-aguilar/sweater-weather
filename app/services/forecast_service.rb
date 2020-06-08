3afb4613cf85fc30f3d43ff963e021be00324f87
Javier Aguilar <jvr.agx@gmail.com>
2020-06-07 23:56:42 -0600

    Add param for imperial

--- app/services/forecast_service.rb
+++ app/services/forecast_service.rb
@@ -2,7 +2,7 @@ class ForecastService
   def forecast_info(location)
     params = { lat: location.lat,
                lon: location.lng,
-               # units: 'imperial',
+               units: 'imperial',
                exclude: 'minutely',
                appid: ENV['OWEATHER_API_KEY'] }
     get_json('onecall', params)
