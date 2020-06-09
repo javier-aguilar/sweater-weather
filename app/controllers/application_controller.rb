6e2158fcee2b9602fff5f3ab70b27d567c639853
Javier Aguilar <jvr.agx@gmail.com>
2020-06-08 18:47:36 -0600

    Add require api key method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  def require_api_key
+    user = User.find_by(api_key: params[:api_key])
+    return error if user.nil?
+  end
+
+  private
+
+  def error
+    render json: { status: :unauthorized,
+                   code: 401,
+                   message: 'Invalid api key' }, status: :unauthorized
+  end
 end
