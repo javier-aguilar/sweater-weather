76d0d1d519f45bde147bac46bcac7c3e09cea54e
Javier Aguilar <jvr.agx@gmail.com>
2020-06-09 21:19:32 -0600

    Add param check

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::BackgroundsController < ApplicationController
   def index
+    return error(:bad_request, 400, 'Location required') unless backgrounds_params[:location]
+
     background = Background.find("#{backgrounds_params[:location]} location")
     render json: BackgroundSerializer.new(background)
   end
